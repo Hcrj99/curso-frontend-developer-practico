7ca44bf939c3800b61eac1d9ac6e59a3b550c48b
camilo <camilo.rodriguez@gatria.co>
2023-08-17 14:04:18 -0500

    Agregar menu desktop en mobile

--- curso-frontend-developer-practico/main.js
+++ curso-frontend-developer-practico/main.js
@@ -1,9 +1,16 @@
 const Menu_email = document.querySelector('.navbar-email');
-const desktopMenu = document.querySelector('.desktop-menu');
+const Desktop_menu = document.querySelector('.desktop-menu');
+const Hamburguer_menu = document.querySelector('.menu');
+const Mobile_menu = document.querySelector('.mobile-menu');
 
-Menu_email.addEventListener('click', toggleDesktopMenu);
 
+Menu_email.addEventListener('click', toggleDesktop_menu);
+Hamburguer_menu.addEventListener('click', togglMobile_menu);
 
-function toggleDesktopMenu(){
-    desktopMenu.classList.toggle('inactive');
+function toggleDesktop_menu(){
+    Desktop_menu.classList.toggle('inactive');
+}
+
+function togglMobile_menu(){
+    Mobile_menu.classList.toggle("inactive");
 }
